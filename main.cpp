064ab281ab50cceaebe0ddfe39778d9af611baa1
Noa Or <noaor7@gmail.com>
2017-11-25 23:52:13 +0200

    Added menu to main.cpp
    Signed-off-by: Noa Or <noaor7@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
+
 //#include "GameFlow.h"
 //#include "RegularLogic.h"
 //#include "HumanPlayer.h"
 //#include "ConsoleScreen.h"
 //#include "AIPlayer.h"
-//#include "gtest_src/gtest.h"
+//
 ///**
 // * Noa or
-// * 20838553433
+// * 208385534
 // */
 //int main() {
+//    cout << "Hello!" << '\n' << "Choose how to play othello:" << '\n' <<
+//         "1: 2 Human players." << '\n' << "2: A Human player and an AI player." << endl;
+//    int choice;
+//    cin >> choice;
+//
 //    GameLogic *logic = new RegularLogic();
-//    Player *player1 = new AIPlayer(Cell ::X, logic);
-//    Player *player2= new AIPlayer(Cell ::O, logic);
+//    Player *player1= new HumanPlayer(Cell ::X, logic);
+//    Player *player2= new HumanPlayer(Cell ::O, logic);
 //    Screen *screen = new ConsoleScreen();
+//
+//    if (choice == 2) {
+//        delete(player2);
+//       Player *player2 = new AIPlayer(Cell::O, logic);
+//    }
 //    GameFlow game = GameFlow(logic, player1, player2, screen);
 //    game.playOneTurn();
 //
